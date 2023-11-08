44521909aaf974f3ac310c054318912af7b6bc9a
DESKTOP-U3B690J\nikolov <nikolay.nikolov@ioio.tv>
2023-11-08 16:17:40 +0200

    fix name

--- src/Navigation/Navigation.test.js
+++ src/Navigation/Navigation.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Navigation from './Navigation';
 
-test('renders learn react link', () => {
+test('renders all events link', () => {
   const { getByText } = render(<Navigation />);
   const linkElement = getByText(/All Events/i);
   expect(linkElement).toBeInTheDocument();
